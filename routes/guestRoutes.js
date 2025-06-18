7335eddd34f54ebb8cf65717ba765f07384cb73f
Rathod Manoj Kumar <rathodmanoj2022@gmail.com>
2025-06-18 21:16:02 +0530

    updated the guestroute

--- routes/guestRoutes.js
+++ routes/guestRoutes.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
 const {
   addGuest,
   getGuests,
   updateGuest,
   deleteGuest,
+  bulkUploadGuests,
 } = require("../controllers/guestController");
-const auth = require("../middleware/authMiddleware");
-const { bulkUploadGuests } = require("../controllers/guestController");
-const router = express.Router();
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
 
+const auth = require("../middleware/authMiddleware");
+
+// ✅ Keep bulk route BEFORE others to avoid conflict
+router.post("/bulk/:eventId", auth, upload.single("file"), bulkUploadGuests);
 router.post("/:eventId", auth, addGuest);
 router.get("/:eventId", auth, getGuests);
 router.put("/:id", auth, updateGuest);
 router.delete("/:id", auth, deleteGuest);
-router.post("/bulk/:eventId", auth, upload.single("file"), bulkUploadGuests);
 
 module.exports = router;
