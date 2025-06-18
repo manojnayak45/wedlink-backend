32b57d5ea8f9a7e291f9560782098f17fd63172c
Rathod Manoj Kumar <rathodmanoj2022@gmail.com>
2025-06-18 14:40:38 +0530

    updated authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const cookieOptions = {
   httpOnly: true,
   secure: true, // ✅ always true on Render (uses HTTPS)
   sameSite: "None", // ✅ MUST be "None" for cross-site cookies
-  path: "/api/auth/refresh", // ✅ Optional but recommended for clarity
+  path: "/", // ✅ Optional but recommended for clarity
 };
 
 // ✅ Token Generator
